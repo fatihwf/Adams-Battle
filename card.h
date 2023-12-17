29716e9d90189457c4b3c45849f05e9d5b95acad
fatih <118255414+fatihwf@users.noreply.github.com>
2023-12-17 12:03:26 +0300

    Update card.h: cards now have skillString and cardIndex attribute, change of constructor to 4 parameters

--- card.h
+++ card.h
@@ -4,6 +4,8 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <Windows.h>
+#include <algorithm>
 using namespace std;
 
 class Card
@@ -11,23 +13,25 @@ class Card
     private:
         int hp  ;
         int atk  ;
+
     public:
         string name;
         string element;
         string cardType ;
+        int cardIndex ;
+        string skillString;
 
         void setHp(int newHp) ;
         void setAtk(int newAtk) ;
         void displayCard() ;
         void (*skill)() ;
 
-        Card* readCardsFromFile(string& filename, int size);
 
         // 2 constructors
         Card() ;
-        Card(string n, string e, string t);
+        Card(string n, string e, string t, int i);
 };
 
-
+void viewCards(Card* cards, int size) ;
 
 #endif // CARDS_H
