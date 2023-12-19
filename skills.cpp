57fb9a7279e36a9ddfc100c46efae5b80e2d456b
fatih <118255414+fatihwf@users.noreply.github.com>
2023-12-19 18:47:39 +0300

    FunctionPointer type ile skiller okunuyor

--- skills.cpp
+++ skills.cpp
@@ -1,89 +1,89 @@
 #include <iostream>
 #include "card.h"
 #include "skills.h"
+
 #include <vector>
 using namespace std ;
+typedef void (*FunctionPointer)();
 
-void skill0() // BROMEN's skill
+void skill1() // BROMEN's skill
 {
     cout << "BROMEN ADAM!" ;
 }
 
-void skill0() // Leoxane
+
+void skill2() // Mobayi
 {
-    
+
 }
 
-void skill1() // Mobayi
+void skill3() // Jagu
 {
-    
+
 }
 
-void skill2() // Jagu
+void skill4() // Atreus
 {
-    
+
 }
 
-void skill3() // Atreus
+void skill5() // Marcus Aurelius
 {
-    
+
 }
 
-void skill4() // Marcus Aurelius
+void skill6() // yusuf
 {
-    
+
 }
 
-void skill5() // yusuf
+void skill7() // Mona Lisa
 {
-    
+
 }
 
-void skill6() // Mona Lisa 
+void skill8() // Bedocan
 {
-    
+
 }
 
-void skill7() // Bedocan
+void skill9() // Lorach
 {
-    
+
 }
 
-void skill8() // Lorach
+void skill10() // Umai
 {
-    
+
 }
 
-void skill9() // Umai
+void skill11() // Bebi
 {
-    
+
 }
 
-void skill10() // Bebi
+void getSkills(FunctionPointer* skills, int size)
 {
-    
+    skills[0] = nullptr;
+    skills[1] = skill1;
+    skills[2] = skill2;
+    skills[3] = skill3;
+    skills[4] = skill4;
+    skills[5] = skill5;
+    skills[6] = skill6;
+    skills[7] = skill7;
+    skills[8] = skill8;
+    skills[9] = skill9;
+    skills[10] = skill10;
+    skills[11] = skill11;
+
+
 }
 
-vector<void(*)()> getSkills()
+void assignSkills(Card** cards, FunctionPointer* skills,int size)
 {
-    vector<void(*)()> skills ;
-
-    skills.push_back(skill0) ;
-    skills.push_back(skill1) ;
-    skills.push_back(skill2) ;
-    skills.push_back(skill3) ;
-    skills.push_back(skill4) ;
-    skills.push_back(skill5) ;
-    skills.push_back(skill6) ;
-    skills.push_back(skill7) ;
-    skills.push_back(skill8) ;
-    skills.push_back(skill9) ;
-    skills.push_back(skill10) ;
-    
-
-    return(skills) ;
+    for (int i = 0; i <size; i++)
+    {
+        cards[i]->skill = skills[i];
+    }
 }
-
-
-
-
