19cc6874f74ae11191e2e8673efe77ed2c395656
fatih <118255414+fatihwf@users.noreply.github.com>
2023-12-19 18:47:58 +0300

    function implementations

--- skills.h
+++ skills.h
@@ -1,10 +1,30 @@
+#ifndef  SKILLS_H
+#define  SKILLS_H
+
 #include <iostream>
 #include "card.h"
 #include <vector>
-#define SIZE 10
+
+
 using namespace std;
 
-void skill0() ;
+typedef void (*FunctionPointer)();
+
+void skill1() ;
+void skill2() ;
+void skill3() ;
+void skill4() ;
+void skill5() ;
+void skill6() ;
+void skill7() ;
+void skill8() ;
+void skill9() ;
+void skill10() ;
+void skill11() ;
+void skill12() ;
+
+void getSkills(FunctionPointer* skills, int size) ;
+void assignSkills(Card** cards, FunctionPointer* skills, int size) ;
 
 
-
+#endif // SKILLS_H
